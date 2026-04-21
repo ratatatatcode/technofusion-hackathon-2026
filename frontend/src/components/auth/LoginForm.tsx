793ffa705521cafd32f6d861081cd0b091c70e8c
VinnRe <22-03804@g.batstate-u.edu.ph>
2026-04-21 22:05:38 +0800

    Replace label and input into shadcn components

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -1,21 +1,25 @@
 import { AuthForm } from "./AuthForm";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
 export default function LoginForm() {
     return (
         <AuthForm title="Login">
-            <div className="flex flex-col gap-1">
-                <label>Email</label>
-                <input type="text" className="border border-black p-1"/>
-            </div>
-            <div className="flex flex-col gap-1">
-                <label>Password</label>
-                <input type="password" className="border border-black p-1"/>
-            </div>
-            {/* THE COLORS ARE TEMPORARY PLEASE CHANGE THEM INTO WHAT THE COLOR PALLET ON THE THEMES */}
-            <Button type="submit" variant="default" size="lg" className="bg-red-600 hover:bg-red-800 cursor-pointer text-white">
-                Login
-            </Button>
+            <form action="">
+                <div className="flex flex-col gap-2">
+                    <Label>Email</Label>
+                    <Input type="text" className="border border-black p-1"/>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <Label>Password</Label>
+                    <Input type="password" className="border border-black p-1"/>
+                </div>
+                {/* THE COLORS ARE TEMPORARY PLEASE CHANGE THEM INTO WHAT THE COLOR PALLET ON THE THEMES */}
+                <Button type="submit" variant="default" size="lg" className="w-full bg-red-600 hover:bg-red-800 cursor-pointer text-white">
+                    Login
+                </Button>
+            </form>
         </AuthForm>
     )
 }
