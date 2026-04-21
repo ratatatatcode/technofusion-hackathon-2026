ae83ba3ff28565346ae6433c72d0affecce2e319
VinnRe <22-03804@g.batstate-u.edu.ph>
2026-04-21 22:14:26 +0800

    Add auth switching Links

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -2,11 +2,12 @@ import { AuthForm } from "./AuthForm";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import Link from "next/link";
 
 export default function LoginForm() {
     return (
         <AuthForm title="Login">
-            <form action="">
+            <form action="" className="flex flex-col gap-4">
                 <div className="flex flex-col gap-2">
                     <Label>Email</Label>
                     <Input type="text" className="border border-black p-1"/>
@@ -19,6 +20,10 @@ export default function LoginForm() {
                 <Button type="submit" variant="default" size="lg" className="w-full bg-red-600 hover:bg-red-800 cursor-pointer text-white">
                     Login
                 </Button>
+                <Label className="flex flex-row justify-center">
+                    {"Don't"} have an account?
+                    <Link href="/signup" className="text-center text-green-500">Signup.</Link>
+                </Label>
             </form>
         </AuthForm>
     )
--- frontend/src/components/auth/SignupForm.tsx
+++ frontend/src/components/auth/SignupForm.tsx
@@ -2,18 +2,22 @@ import { AuthForm } from "./AuthForm";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import Link from "next/link";
 
 export default function SignupForm() {
     return (
         <AuthForm title="Signup">
-            <form action="">
+            <form action="" className="flex flex-col gap-4">
+                <div className="flex flex-row gap-4">
+                    <div className="flex flex-col gap-2">
+                        <Label>First Name</Label>
+                        <Input type="text" className="border border-black p-1"/>
+                    </div><div className="flex flex-col gap-2">
+                        <Label>Last Name</Label>
+                        <Input type="text" className="border border-black p-1"/>
+                    </div>
+                </div>
                 <div className="flex flex-col gap-2">
-                    <Label>First Name</Label>
-                    <Input type="text" className="border border-black p-1"/>
-                </div><div className="flex flex-col gap-2">
-                    <Label>Last Name</Label>
-                    <Input type="text" className="border border-black p-1"/>
-                </div><div className="flex flex-col gap-2">
                     <Label>Email</Label>
                     <Input type="text" className="border border-black p-1"/>
                 </div>
@@ -29,6 +33,10 @@ export default function SignupForm() {
                 <Button type="submit" variant="default" size="lg" className="w-full bg-green-600 hover:bg-green-800 cursor-pointer text-white">
                     Signup
                 </Button>
+                <Label className="flex flex-row justify-center">
+                    Already have an account?
+                    <Link href="/login" className="text-center text-green-500">Login.</Link>
+                </Label>
             </form>
         </AuthForm>
     )
